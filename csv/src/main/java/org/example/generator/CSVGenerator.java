df01cbb15b2a9f9e0285604699465ce848df555c
Denis Orekhov <3008denis@gmail.com>
2024-09-17 19:17:26 +0300

    Add settings for generator

--- csv/src/main/java/org/example/generator/CSVGenerator.java
+++ csv/src/main/java/org/example/generator/CSVGenerator.java
@@ -10,16 +10,20 @@ import java.util.List;
 import java.util.Map;
 
 public final class CSVGenerator {
-    private Map<String, String> settings;
+    private String delimiter;
+    private boolean includingHeaders;
 
     public CSVGenerator(Map<String, String> settings) {
-        this.settings = settings;
+        delimiter = settings.getOrDefault("delimiter", ", ");
+        includingHeaders = Boolean.parseBoolean(settings.getOrDefault("includingHeaders", "true"));
     }
 
     public <T> void writeDataToFile(List<T> data, String path) {
         var fielPath = Path.of(path);
         try {
-            writeHeaders(fielPath, data.getFirst());
+            if (includingHeaders) {
+                writeHeaders(fielPath, data.getFirst());
+            }
 
             for (var obj : data) {
                 writeObjToFile(fielPath, obj);
@@ -38,7 +42,7 @@ public final class CSVGenerator {
         for (var field : fields) {
             if (field.isAnnotationPresent(CSVColumn.class)) {
                 var annotation = field.getAnnotation(CSVColumn.class);
-                sb.append(annotation.header()).append(", ");
+                sb.append(annotation.header()).append(delimiter);
             }
         }
 
@@ -53,7 +57,7 @@ public final class CSVGenerator {
         for (var field : fields) {
             if (field.isAnnotationPresent(CSVColumn.class)) {
                 field.setAccessible(true);
-                sb.append(field.get(obj)).append(", ");
+                sb.append(field.get(obj)).append(delimiter);
             }
         }
         Files.writeString(path, sb.substring(0, sb.length() - 1) + System.lineSeparator(),
